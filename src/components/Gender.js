7d77223fd0d7533989c90b683ca080d1f333f55f
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2023-07-22 16:49:43 +0530

    submitting code-Sat Jul 22 2023 16:49:40 GMT+0530 (India Standard Time)

--- src/components/Gender.js
+++ src/components/Gender.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 
 
 const Gender = ()=>{
-    let [item,setItem]=useState("");
+    let [item,setItem]=useState([]);
 
 
     function genderValue(event){
-        if(event.target.value === "male"){
-            setItem(...item,item.name="shirt",item.size[0]="Small",item.size[0]="Medium",item.size[0]="Large");
-        }
-        else{
-            setItem(...item,item.name="dress",item.size[0]=2,item.size[0]=4,item.size[0]=4);
+        event.preventDefault();
+        if(event.target.value=="male"){
+            setItem([event.target.value,"shirt"]);
+        }else{
+            setItem([event.target.value,"dress"]);
         }
     }
 
@@ -18,19 +18,28 @@ const Gender = ()=>{
         <div>
            <div>
             <h2>Select your gender:</h2>
-            <input type="radio" value="male" name="gender" onChange={genderValue}>Male</input>
-            <input type="radio" value="female" name="gender" onChange={genderValue}>Female</input>
+            <input type="radio" value="male" name="gender" onClick={genderValue}/>Male
+            <input type="radio" value="female" name="gender" onClick={genderValue}/>Female
            </div>
 
-           <div>
-            <h2>Select your {item} size</h2>
+           <h2>Select your {item[1]} size</h2>
+           {item[0]==="male" &&
             <select>
                 <option>Select size</option>
-                <option></option>
-                <option></option>
-                <option></option>
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
             </select>
-           </div>
+            }
+            
+            {item[0]==="female" && 
+            <select>
+                <option>Select size</option>
+                <option>2</option>
+                <option>4</option>
+                <option>6</option>
+            </select>
+            }
         </div>
     );
 }
